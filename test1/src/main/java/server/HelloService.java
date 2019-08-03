d0a4f08a0e3e9a4a9eb1d38dea32ddfcbed3a9fb
zhangfengda <mumubeyond@163.com>
2019-08-03 16:34:17 +0800

    修改项目名称

--- test1/src/main/java/server/HelloService.java
+++
@@ -1,9 +0,0 @@
-package server;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface HelloService extends Remote {
-
-    String hello(String name) throws RemoteException;
-}
