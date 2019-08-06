06ec5eb6acc30d101cc506b33baf2d9aa366a5d9
zhangfengda <mumubeyond@163.com>
2019-08-06 18:21:00 +0800

    bugfix

--- rpcServer/src/main/java/rpc/serverDemo.java
+++ rpcServer/src/main/java/rpc/serverDemo.java
@@ -4,7 +4,7 @@ public class serverDemo {
     public static void main(String[] args) {
         IGPHello iGPHello = new GPHelloImpl();
         RpcServer rpcServer = new RpcServer();
-        //发布服务
+        //发布服务 应用和端口号
         rpcServer.publisher(iGPHello,8000);
     }
 }
